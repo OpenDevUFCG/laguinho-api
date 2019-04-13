bbd5a576fe58ae0c5b327040ceef2ce1e2c777cd
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2019-04-13 18:09:41 -0300

    Adiciona dados de relações entre as disciplinas

--- src/v1/subjects/subj-controller.js
+++ src/v1/subjects/subj-controller.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import loadSubjects from './subj-data';
+import { loadSubjects, loadSubjectsRelationships } from './subj-data';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     res.json(await loadSubjects());
+});
+
+router.get('/relationships', async (req, res) => {
+    res.json(await loadSubjectsRelationships());
 });
 
 export default router;
--- src/v1/subjects/subj-data.js
+++ src/v1/subjects/subj-data.js
@@ -1,7 +1,19 @@
 import csvtojson from 'csvtojson';
 import axios from 'axios';
 
-export default async function loadSubjects() {
-    const csvFilePath = 'https://raw.githubusercontent.com/OpenDevUFCG/roadmap-cc/master/data/disciplinas-ppc-2017.csv';
+const basePath = 'https://raw.githubusercontent.com/OpenDevUFCG/roadmap-cc/development/data'
+
+async function loadSubjects() {
+    const csvFilePath = `${basePath}/disciplinas-ppc-2017.csv`;
     return await csvtojson().fromString((await axios.get(csvFilePath)).data)
+}
+
+async function loadSubjectsRelationships() {
+    const csvFilePath = `${basePath}/relacionamentos-disciplinas-ppc2017.csv`;
+    return await csvtojson().fromString((await axios.get(csvFilePath)).data)
+}
+
+export {
+    loadSubjects,
+    loadSubjectsRelationships
 }
