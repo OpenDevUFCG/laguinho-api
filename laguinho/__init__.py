d0f33855f8d0d750649580c5118cc2702897522d
LukeHxH <lucasmed812@gmail.com>
2019-09-03 14:42:55 -0300

    Resolve alguns erros e renomeia script de start para dev

--- laguinho/__init__.py
+++ laguinho/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 def create_app(config_filename=None):
     app = Flask(__name__)
+    
     @app.route('/')
     def hello_world():
         return 'Laguinho API is up and running.'
