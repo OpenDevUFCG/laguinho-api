6c2e6a3ed321d2f84f0b7b9b3e45f886dab56416
lucasaarcoverde <lucasaarcoverde@gmail.com>
2019-09-06 21:15:51 -0300

    Padroniza a rota com o REST

--- laguinho/routes/datasets.py
+++ laguinho/routes/datasets.py
@@ -2,16 +2,16 @@ from flask import Blueprint, request, jsonify
 from laguinho.models.metadata import MetadataSchema
 from marshmallow import ValidationError, EXCLUDE
 
-datasets = Blueprint('datasets', __name__, url_prefix="/datasets")
+datasets = Blueprint('datasets', __name__)
 metadata = []
 
-@datasets.route("/publish", methods=['POST'])
+@datasets.route("/datasets", methods=['POST'])
 def publish():
     schema = MetadataSchema()
     result = schema.load(request.json, unknown=EXCLUDE)
     metadata.append(result)
     return result, 201
 
-@datasets.route("/metadata", methods=['GET'])
+@datasets.route("/datasets", methods=['GET'])
 def get_metadata():
     return jsonify(metadata)
