f20fa145b8a9098c09308daa8f8a6ce3a67c08e7
lucasaarcoverde <lucasaarcoverde@gmail.com>
2019-09-06 21:27:48 -0300

    Ajusta identação para padronizar com o PEP8

--- laguinho/models/metadata.py
+++ laguinho/models/metadata.py
@@ -4,9 +4,18 @@ def get_required_error(attribute):
         return { "required": { "message": attribute + " is required" } }
         
 class DatasetMetadataSchema(Schema):
-    name = fields.Str(required=True, error_messages=get_required_error("name"))
-    url = fields.Str(required=True, error_messages=get_required_error("url"))
-    path = fields.Str(required=True, error_messages=get_required_error("path"))
+    name = fields.Str(
+        required=True, 
+        error_messages=get_required_error("name"))
+    url = fields.Str(
+        required=True, 
+        error_messages=get_required_error("url"))
+    path = fields.Str(
+        required=True, 
+        error_messages=get_required_error("path"))
     maintainers = fields.List(fields.Str())
-    format = fields.Str(validate=validate.OneOf(["json", "csv"]), required=True, error_messages=get_required_error("format"))
+    format = fields.Str(
+        validate=validate.OneOf(["json", "csv"]), 
+        required=True, 
+        error_messages=get_required_error("format"))
     contributable = fields.Bool()
