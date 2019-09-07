6cbdc98869efaea4dfb93c08a9df2b288cd5b311
lucasaarcoverde <lucasaarcoverde@gmail.com>
2019-09-06 21:17:20 -0300

    Ajusta nome da função para pegar datasets

--- laguinho/routes/datasets.py
+++ laguinho/routes/datasets.py
@@ -3,15 +3,15 @@ from laguinho.models.metadata import MetadataSchema
 from marshmallow import ValidationError, EXCLUDE
 
 datasets = Blueprint('datasets', __name__)
-metadata = []
+datasets_metadata = []
 
 @datasets.route("/datasets", methods=['POST'])
 def publish():
     schema = MetadataSchema()
     result = schema.load(request.json, unknown=EXCLUDE)
-    metadata.append(result)
+    datasets_metadata.append(result)
     return result, 201
 
 @datasets.route("/datasets", methods=['GET'])
-def get_metadata():
-    return jsonify(metadata)
+def get_datasets():
+    return jsonify(datasets_metadata)
