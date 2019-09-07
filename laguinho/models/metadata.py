04998a108e4c1f31ff6e0be39c6e45f6d270c61b
Lucas de Medeiros <lucasmed812@gmail.com>
2019-09-07 09:41:00 -0300

    Remove import não utilizado (pprint)

--- laguinho/models/metadata.py
+++ laguinho/models/metadata.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields, pprint, validate
+from marshmallow import Schema, fields, validate
 
 def get_required_error(attribute):
         return {"required": {"message": attribute + " is required"}}
