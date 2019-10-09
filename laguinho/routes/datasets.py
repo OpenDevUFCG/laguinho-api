edc289b69d44065910461681009ee3be3fcc8661
paulojbleitao <paulojbleitao@gmail.com>
2019-10-09 15:57:13 -0300

    Adiciona checagem de dataset duplicado

--- laguinho/routes/datasets.py
+++ laguinho/routes/datasets.py
@@ -5,9 +5,20 @@ from marshmallow import ValidationError, EXCLUDE
 datasets = Blueprint('datasets', __name__)
 datasets_metadata = []
 
+def equals(dataset1, dataset2):
+    return (dataset1['url'] == dataset2['url'] and
+            dataset1['path'] == dataset2['path'])
+
+def dataset_exists(incoming_dataset):
+    compare_datasets = lambda dataset: equals(dataset, incoming_dataset)
+    filtered = list(filter(compare_datasets, datasets_metadata))
+    return len(filtered) > 0
+
 @datasets.route("/datasets", methods=['POST'], strict_slashes=False)
 def publish():
     result = dataset_metadata.load(request.json, unknown=EXCLUDE)
+    if dataset_exists(result):
+        return jsonify('Dataset already exists'), 409
     datasets_metadata.append(result)
     return jsonify(result), 201
 
